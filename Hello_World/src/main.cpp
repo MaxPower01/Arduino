0bd3ff9e0d76d456594933ba28606f328a0ed936
Maxime Manseau <m.maxime01@outlook.com>
2019-02-03 09:55:05 -0500

    Update

--- Hello_World/src/main.cpp
+++ Hello_World/src/main.cpp
@@ -9,17 +9,12 @@
 |  ==============> 1. Variables
 \* ============================================================ */ 
 
-// Boutons :
-const int BUTTON = 2;
-const int LASER = 3;
-
-// Capteurs de son :
-const int SOUND_SENSOR_1 = A0;
-const int SOUND_SENSOR_2 = A1;
-const int SOUND_SENSOR_3 = A2;
-const int SOUND_SENSOR_4 = A3;
-const int SOUND_SENSOR_5 = A4;
-const int SOUND_SENSOR_6 = A5;
+// Lasers :
+const int LASER_1 = 2;
+const int LASER_2 = 4;
+const int LASER_3 = 7;
+const int LASER_4 = 10;
+const int LASER_5 = 13;
 
 // État des boutons :
 bool button = false;
@@ -28,19 +23,6 @@ bool button = false;
 bool systemArmed = false;
 bool alarm = false;
 int alarmLevel = 0;
-
-// Moyenne d'échantillons :
-const int INPUTS = 6;
-const byte inputPins[INPUTS] = {A0, A1, A2, A3, A4, A5};
-
-const int READINGS = 20;          // Nombre d'échantillons à prendre en compte
-int threshold = 1;                // Niveau de sensibilité de la détection
-int readings[INPUTS][READINGS];   // Lectures provenant des capteurs
-int readIndex[INPUTS] = {0, 0};   // Index de la lecture en cours
-int total[INPUTS] = {0, 0};       // Somme des échantillons
-int average[INPUTS] = {0, 0};     // Moyenne des échantillons
-
-
 
 
 
@@ -54,24 +36,12 @@ void setup() {
 
   // Définition du mode de chacune des pattes utilisées :
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(BUTTON, INPUT);
-  pinMode(LASER, OUTPUT);
-  pinMode(SOUND_SENSOR_1, INPUT);
-  pinMode(SOUND_SENSOR_2, INPUT);
-  pinMode(SOUND_SENSOR_3, INPUT);
-  pinMode(SOUND_SENSOR_4, INPUT);
-  pinMode(SOUND_SENSOR_5, INPUT);
-  pinMode(SOUND_SENSOR_6, INPUT);
-
-  // Initialisation de toutes les lectures d'échantillons à 0 :
-  for (int i = 0; i < INPUTS; i++) {
-    for (int thisReading = 0; thisReading < READINGS; thisReading++) {
-      readings[i][thisReading] = 0;
-    }
-  }
+  pinMode(LASER_1, OUTPUT);
+  pinMode(LASER_2, OUTPUT);
+  pinMode(LASER_3, OUTPUT);
+  pinMode(LASER_4, OUTPUT);
+  pinMode(LASER_5, OUTPUT);
 }
-
-
 
 
 
@@ -79,70 +49,12 @@ void setup() {
 |  ==============> 4. Methods
 \* ============================================================ */ 
 
-void armSystem() {
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  systemArmed = true;
-}
-
-void checkButton() {
-  if (digitalRead(BUTTON) == HIGH) {
-    button = true;
-  } else {
-    button = false;
-  }
-}
-
-void averageSamples() {
-  for (int i = 0; i < INPUTS; i++) {
-    // Soustraction de la dernière lecture :
-    total[i] = total[i] - readings[i][readIndex[i]];
-
-    // Lecture d'un échantillon provenant du capteur :
-    readings[i][readIndex[i]] = analogRead(inputPins[i]);
-
-    // Ajout de cet échantillon au total :
-    total[i] = total[i] + readings[i][readIndex[i]];
-
-    // On passe à la position suivante :
-    readIndex[i] = readIndex[i] + 1;
-
-    // Si on est à la fin du tableau, on recommence :
-    if (readIndex[i] >= READINGS) {
-      readIndex[i] = 0;
-    }
-
-    // Calcul de la moyenne des échantillons pour le capteur "i" :
-    average[i] = total[i] / READINGS;
-  }
-}
-
-void flash(int pin, int interval) {
-    digitalWrite(pin, HIGH);
+void flash(int digitalPin, int interval) {
+    digitalWrite(digitalPin, HIGH);
     delay(interval);
-    digitalWrite(pin, LOW);
+    digitalWrite(digitalPin, LOW);
     delay(interval);
 }
-
-void triggerAlarm() {
-  for(int i = 0; i < alarmLevel; i++) {
-    flash(LED_BUILTIN, 250);
-  }
-}
-
-void watchSamples() {
-  for (int i = 0; i < INPUTS; i++) {
-    if (average[i] >= threshold && systemArmed) {
-      // Le niveau d'alarme augmente à chaque fois qu'un son est détecté pendant que le système est armé :
-      systemArmed = false;
-      alarmLevel = alarmLevel + 1;
-      triggerAlarm();
-      systemArmed = true;
-    }
-  }
-}
-
-
 
 
 
@@ -151,7 +63,5 @@ void watchSamples() {
 \* ============================================================ */ 
 
 void loop() {
-  flash(LED_BUILTIN, 10);
-  flash(LASER, 10);
-  delay(1);
+  delay(500);
 }
