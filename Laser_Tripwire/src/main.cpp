59bd1ade2e2e5d67634a04de259a5e66b51f29ae
Maxime Manseau <m.maxime01@outlook.com>
2019-02-21 02:03:30 -0500

    Ajustement du fil de déclenchement au laser

--- Laser_Tripwire/src/main.cpp
+++ Laser_Tripwire/src/main.cpp
@@ -1,33 +1,25 @@
 #include <Arduino.h>
+#include <RH_ASK.h>
+#include <SPI.h> // Not actually used but needed to compile
 
 // 1. Variables
 // 2. Setup
-// 3. Fonctions
+// 3. Méthodes
 // 4. Loop
 
 /* ============================================================ *\ 
 |  ==============> 1. Variables
 \* ============================================================ */ 
 
-// Boutons :
-const int BUTTON_RED = 12;
+RH_ASK driver;
 
-bool buttonRed = false;
+const int BUTTON = 5;
 
-// Lumières LED :
-const int LED_GREEN = 2;
-const int LED_BLUE = 3;
-const int LED_RED = 4;
-
-// Capteur de lumière :
 const int LIGHT_SENSOR = A5;
-
 int light;
 
-// Laser :
-const int LASER = 8;
+const int LASER = 2;
 
-// Système d'alarme :
 bool systemArmed = false;
 
 bool alarm = false;
@@ -35,7 +27,7 @@ int alarmLevel = 1;
 
 // Moyenne d'échantillons :
 const int SMOOTHNESS = 3;     // Nombre d'échantillons à prendre en compte
-const int THRESHOLD = 750;  // Niveau de sensibilité de la détection
+const int THRESHOLD = 100;  // Niveau de sensibilité de la détection
 
 int readings[SMOOTHNESS];     // Lectures provenant des capteurs
 int readIndex = 0;          // Index de la lecture en cours
@@ -54,18 +46,14 @@ void setup() {
   // Initialisation de la communication à un débit de 9600 bits/seconde :
   Serial.begin(9600);
 
+  if (!driver.init())
+      Serial.println("init failed");
+
   // Définition du mode de chacune des pattes utilisées :
   pinMode(LED_BUILTIN, OUTPUT);
-
-  pinMode(LED_GREEN, OUTPUT);
-  pinMode(LED_BLUE, OUTPUT);
-  pinMode(LED_RED, OUTPUT);
-  
-  pinMode(BUTTON_RED, INPUT);
-
-  pinMode(LIGHT_SENSOR, INPUT);
-
+  pinMode(BUTTON, INPUT);
   pinMode(LASER, OUTPUT);
+  pinMode(LIGHT_SENSOR, INPUT);
 }
 
 
@@ -73,64 +61,19 @@ void setup() {
 
 
 /* ============================================================ *\ 
-|  ==============> 3. Fonctions
+|  ==============> 3. Méthodes
 \* ============================================================ */
 
-void armSystem() {
-  if (alarmLevel >= 4) {
-    digitalWrite(2, LOW);
-    digitalWrite(3, LOW);
-    digitalWrite(4, LOW);
-    
-    delay(1000);
-    
-    digitalWrite(LED_BUILTIN, HIGH);
-    
-    systemArmed = true;
-    alarmLevel = 1;
-
-  } else {
-    delay(1000);
-    digitalWrite(LED_BUILTIN, HIGH);
-    systemArmed = true;
-  }
+void armSystem() {       
+  digitalWrite(LASER, HIGH);
+  delay(1000);
+  systemArmed = true;
 }
 
-void checkButtons() {
-  // Vérifie si un bouton est appuyé :
-  if (digitalRead(BUTTON_RED) == HIGH) {
-    buttonRed = true;
-  } else {
-    buttonRed = false;
-  }
-}
-
-void smoothing() {
-    // subtract the last reading:
-  total = total - readings[readIndex];
-  // read from the sensor:
-  readings[readIndex] = analogRead(LIGHT_SENSOR);
-  // add the reading to the total:
-  total = total + readings[readIndex];
-  // advance to the next position in the array:
-  readIndex = readIndex + 1;
-
-  // if we're at the end of the array...
-  if (readIndex >= SMOOTHNESS) {
-    // ...wrap around to the beginning:
-    readIndex = 0;
-  }
-
-  average = total / SMOOTHNESS;
-}
-
-void triggerAlarm(int led) {
-  int previousLed = led - 1;
-
+void disarmSystem() {
+  systemArmed = false;
   digitalWrite(LASER, LOW);
-  digitalWrite(LED_BUILTIN, LOW);
-  digitalWrite(led, HIGH);
-  digitalWrite(previousLed, LOW);
+  delay(1000);
 }
 
 
@@ -142,29 +85,26 @@ void triggerAlarm(int led) {
 \* ============================================================ */ 
 
 void loop() {
-
-  checkButtons();
-
-  if (buttonRed) {
-    digitalWrite(LASER, HIGH);
+  
+  
+  // Activation ou désactivation du fil de déclenchement au laser :
+  if (digitalRead(BUTTON) == HIGH && systemArmed == false)
+  {
     armSystem();
   }
-  
-  light = analogRead(LIGHT_SENSOR);
-
-  // Établissement de la moyenne :
-  // averageSamples();
-  
-  // Serial.print("La moyenne des 5 derniers échantillons est : ");
-  // Serial.println(average);
-  Serial.println(light);
-
-  if (light <= THRESHOLD && systemArmed) {
-    systemArmed = false;
-    alarmLevel = alarmLevel + 1;
-    
-    triggerAlarm(alarmLevel);
+  else if (digitalRead(BUTTON) == HIGH && systemArmed == true)
+  {
+    disarmSystem();
   }
 
-  delay(1);
+  light = analogRead(LIGHT_SENSOR);
+  Serial.println(light);
+
+  if (light <= THRESHOLD && systemArmed)
+  {
+    const char *msg = "Hello World!";
+    driver.send((uint8_t *)msg, strlen(msg));
+    driver.waitPacketSent();
+    delay(1000);
+  }
 }
---
+++ RF_433MHz_Transmitter_and_Receiver/src/receiver.cpp
@@ -0,0 +1,62 @@
+#include <Arduino.h>
+#include <RH_ASK.h>
+#include <SPI.h> // Not actualy used but needed to compile
+
+RH_ASK driver;
+
+const int BLUE_LED = 7;
+const int RED_LED = 8;
+
+int alarm = 0;
+
+void setup() {
+  Serial.begin(9600); // Debugging only
+
+  pinMode(BLUE_LED, OUTPUT); 
+  pinMode(RED_LED, OUTPUT); 
+  
+  if (!driver.init()) {
+    Serial.println("init failed");  
+  }
+}
+
+void loop() {
+  uint8_t buf[12];
+  uint8_t buflen = sizeof(buf);
+  
+  // Non-blocking
+  if (driver.recv(buf, &buflen)) {
+    int i;
+
+    // Message with a good checksum received, dump it.
+    Serial.print("Message: ");
+    Serial.println((char*)buf);
+
+    if (alarm < 2) {
+      alarm = alarm + 1;
+    } else if (alarm >= 2) {
+      alarm = 0;
+    }
+
+    switch (alarm)
+    {
+      case 0:
+        digitalWrite(BLUE_LED, LOW);
+        digitalWrite(RED_LED, LOW);
+        break;
+
+      case 1:
+        digitalWrite(RED_LED, LOW);
+        digitalWrite(BLUE_LED, HIGH);
+        break;
+
+      case 2:
+        digitalWrite(BLUE_LED, LOW);
+        digitalWrite(RED_LED, HIGH);
+        break;
+    
+      default:
+        break;
+    }
+  }
+}
