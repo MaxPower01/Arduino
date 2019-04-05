b668b03ebf78a1e3912c353664615df5ea8c7ad7
Maxime Manseau <m.maxime01@outlook.com>
2019-04-05 19:00:10 -0400

    Ajout de 2 librairies et création du sketch "keypad-4-x-4"

---
+++ La_Nuit_des_Voleurs/lib/keypad-4-x-4.cpp
@@ -0,0 +1,14 @@
+// -------------------- KEYPAD 4X4 -------------------- //
+
+#include <Arduino.h>
+#include <Keypad.h>
+#include <Password.h>
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+}
